dbc34d162d7f79596571acace9dad37c272601db
braddy66 <ssodhi20000@gmail.com>
2024-04-24 23:10:05 -0500

    scooby dooby do I love u

--- graphintro/src/Scooby.java
+++ graphintro/src/Scooby.java
@@ -18,12 +18,12 @@ public class Scooby {
     }
     public static void main(String[] args) throws Exception {
         File f = new File("scooby.dat");
-        HashMap<Character, Node> hm = new HashMap<>();
         Scanner sc = new Scanner(f);
         int N = sc.nextInt();
         sc.nextLine();
         while(N-- > 0){
             String s = sc.nextLine();
+            HashMap<Character, Node> hm = new HashMap<>();
             int i = 0;
             while(i < s.length()){
                 char x = s.charAt(i);
@@ -32,14 +32,17 @@ public class Scooby {
                 if(!hm.containsKey(x)) hm.put(x, new Node(x));
                 if(!hm.containsKey(y)) hm.put(y, new Node(y));
                 hm.get(x).al.add(hm.get(y));
+                hm.get(y).al.add(hm.get(x));
                 i+=2;
             }
             s = sc.nextLine();
             Node start = hm.get(s.charAt(0));
             Node end = hm.get(s.charAt(1));
-            visited = new HashSet<>(); 
-            if(path(start, end)) System.out.println("yes");
-            else System.out.println("no");
+            visited = new HashSet<>();
+            visited.add(start); 
+            if( start == null || end == null ||!path(start, end)) 
+            System.out.println("no");
+            else System.out.println("yes");
         }
     }
     static boolean path(Node x, Node y){
@@ -47,10 +50,9 @@ public class Scooby {
         ArrayList<Node> al = x.al;
         visited.add(x);
         for(int i = al.size()-1; i > -1;i--){
-            if(!visited.contains(al.get(i))){
-                if(path(al.get(i), y)) return true;
-            } 
-            visited.add(al.get(i));
+            if(!visited.contains(al.get(i)) && path(al.get(i), y)){
+                return true; 
+            }
         }
         return false;
     }
